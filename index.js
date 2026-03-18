1b710afd4361085a793a5a1b9751982ce0ac41bc
Shelley <Shelleylee688@gmail.com>
2026-03-17 23:08:28 -0400

    Added hover color

---
+++ index.js
@@ -0,0 +1,13 @@
+// Adding an event listener to an element
+    // Caching the element
+let btn = document.getElementById('SubmitButton')
+    // Add an event listener
+btn.addEventListener(
+    'click', function () {
+        alert('Message Sent!')
+    })
+
+/* document.addEventListener("DOMContentLoaded", function () {
+  const form = document.querySelector("form");
+  const successAlert = document.getElementById("successAlert");
+}) */
